b3c080326114edeffe794686cc63d6858baab1b3
Amir Reza Qasemi <83636447+AR-Q@users.noreply.github.com>
2024-09-01 11:36:21 +0330

    Test api

---
+++ TestAPI/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        
+
+
+        [HttpGet]
+        public ActionResult<Person> Test()
+        {
+            Person person = new Person() { Name = "amir", Password = "1234" };
+
+            return person;
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
